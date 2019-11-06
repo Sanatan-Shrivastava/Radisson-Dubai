e577bc6582361f159d67cbb46bdba20451c92901
Sanatan-Shrivastava <sanatan.cseiiitk@gmail.com>
2019-11-07 05:23:30 +0530

    added gold

--- src/components/BanGold.js
+++ src/components/BanGold.js
@@ -3,16 +3,18 @@ import pic13 from '../assets/images/gold1.jpg'
 
 const BanGold = (props) => (
     <section style={{backgroundImage:`url(${pic13})`}}  id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h3 style={{fontFamily:'Cinzel Decorative'}} >With Gold, get the Golden Dreams along with a lots of Savings!</h3>
+        <div  style={{color:'#d4af37'}} className="inner">
+            <header  style={{color:'#d4af37'}} className="">
+            
+                <h3 style={{fontFamily:'Cinzel Decorative', color:'#d4af37'}} >With Gold, get the Golden Dreams along with a lots of Savings!</h3>
             </header>
+            <hr color="#d4af37"/>
             <div className="content">
-                <p>Planning your most Vibrant and golden stays</p>
+                <p style={{color:'#d4af37'}}>Planning your most Vibrant and golden stays</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Details</a></li>
-                    <li><a href="#two" className="button next scrolly">Book Now</a></li>
-                    <li><a href="#three" className="button next scrolly">Gallery</a></li>
+                    <li><a style={{color:'#d4af37'}} href="#one" className="button next scrolly">Details</a></li>
+                    <li><a style={{color:'#d4af37'}} href="#two" className="button next scrolly">Book Now</a></li>
+                    <li><a  style={{color:'#d4af37'}} href="#three" className="button next scrolly">Gallery</a></li>
                 </ul>
             </div>
         </div>
